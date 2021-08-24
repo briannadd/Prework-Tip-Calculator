915b40c36ec839d5df954c49632d9e422ee1a027
Brianna Decker-Diaz <brianandeckerdiaz@gmail.com>
2021-08-24 15:04:20 -0500

    Initial Commit

---
+++ Prework Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework Tip Calculator
+//
+//  Created by Brianna Decker-Diaz on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
